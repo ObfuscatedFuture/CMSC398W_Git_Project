20c8e2e1e2eb6f135f20495ec650f6fdb55cf7e5
shara-alam <salam126@terpmail.umd.edu>
2026-03-25 17:33:59 -0400

    Added a module docstring to the validator file

--- src/validator.py
+++ src/validator.py
@@ -1,3 +1,10 @@
+"""
+This file has three validation functions. The first one, validate_number, will
+check that a given value can be converted into a number. The second function, 
+validate_operation, will check that a given operation is accepted by the
+calculator. The third function, validate_non_negative, will check that a given
+number is positive.
+"""
 def validate_number(value):
     """Validate that value can be converted to a number."""
     try:
